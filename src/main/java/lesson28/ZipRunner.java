1577b927bf029566394c37be7ccc3e4d5417e1c1
vaikcode <vaikcode@gmail.com>
2022-03-23 17:20:35 +0900

    Add lesson28

---
+++ src/main/java/lesson28/ZipRunner.java
@@ -0,0 +1,16 @@
+package lesson28;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@SpringBootApplication
+public class ZipRunner {
+
+    public static void main(String[] args) {
+        final ConfigurableApplicationContext applicationContext = SpringApplication.run(ZipRunner.class, args);
+
+
+
+    }
+}
